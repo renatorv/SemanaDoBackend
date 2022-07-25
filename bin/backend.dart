8baf90451abed2d129b02d5858c49bfd92410c48
renatorv <renatorv@gmail.com>
2022-07-25 16:14:21 -0300

    Semana do backend #5 - Rotas com shelf_modular

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,34 +1,10 @@
-import 'dart:async';
-
-import 'package:shelf/shelf.dart';
+import 'package:backend/backend.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
 void main() async {
-  var pipeline = Pipeline().addMiddleware(log());
+  final handler = await startShelfModular();
 
-  final server = await io.serve( pipeline.addHandler(_handler),
-    '0.0.0.0',
-    4470,
-  );
+  final server = await io.serve(handler, '0.0.0.0', 4471);
 
-  print('Online: ${server.address.address}:${server.port}');
-}
-
-Middleware log() {
-  return (handler) {
-    return (request) async {
-      // antes de executar
-      print('Solicitado: ${request.url}');
-      var response = await handler(request);
-      print('[${response.statusCode}] - Response');
-
-      // depois de executar
-      return response;
-    };
-  };
-}
-
-FutureOr<Response> _handler(Request request) {
-  print(request);
-  return Response(200, body: 'Corpo......');
+  print('Server on-line: ${server.address.address}:${server.port}');
 }
--- lib/backend.dart
+++ lib/backend.dart
@@ -1,3 +1,13 @@
-int calculate() {
-  return 6 * 7;
+import 'package:backend/src/app_module.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_modular/shelf_modular.dart';
+
+startShelfModular() async {
+  final handler = Modular(
+    module: AppModule(),
+    middlewares: [
+      logRequests(),
+    ],
+  );
+  return handler;
 }
