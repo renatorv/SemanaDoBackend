a9d0e342d380b1c66e6e2ffab298c4c3788b252a
renatorv <renatorv@gmail.com>
2022-07-28 08:52:45 -0300

    finalizada aula 7

---
+++ lib/src/core/services/dot_env/dot_env_service.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+class DotEnvService {
+  final Map<String, String> _map = {};
+
+  static DotEnvService instance = DotEnvService._();
+  DotEnvService._() {
+    _init();
+  }
+
+  void _init() {
+    final file = File('.env');
+
+    final envText = file.readAsStringSync();
+
+    for (var line in envText.split('\n')) {
+      final lineBreak = line.split('=');
+      _map[lineBreak[0]] = lineBreak[1];
+    }
+  }
+
+  // String? getValue(String key) {
+  //   return _map[key];
+  // }
+
+  // ou [ Semana do backend #7 - Técnica DotEnv(.env) :: 7 minutos ]
+  String? operator [](String key) {
+    return _map[key];
+  }
+}
